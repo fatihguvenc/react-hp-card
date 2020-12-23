33f5e5b601c85e7c68176bea1f2b1ac4eebabdf8
fatihgvnc <heatclife007@gmail.com>
2020-12-23 17:20:18 +0300

    1 file modified

--- src/components/likeButton/UnLike.jsx
+++ src/components/likeButton/UnLike.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function UnLike(props) {
   return (
-    <button onClick={props.onClick} type="button" class="btn btn-danger">
+    <button onClick={props.onClick} type="button" className="btn btn-danger">
       <i class="fas fa-thumbs-down" />
     </button>
   );
